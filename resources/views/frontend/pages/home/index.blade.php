d195b469c216a382e2b8e6da29765913221fff83
stevephamhi <hung.pham@cubicstack.net>
2023-09-27 09:23:11 +0700

    fe homepage p2

--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -1,1 +1,33 @@
 @extends('frontend.layouts.master')
+
+@push('style_pages')
+<link rel="stylesheet" href="{{ asset('backoffice/assets/vendors/general/slick/slick.css') }}">
+<link rel="stylesheet" href="{{ asset('backoffice/assets/vendors/general/slick/slick-theme.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/assets/css/pages/home/index.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/assets/css/common/component-slider.css') }}">
+@endpush
+
+@section('content_body')
+    @include('frontend.pages.home.parials.section-banner')
+@endsection
+
+@push('js_pages')
+<script src="{{ asset('backoffice/assets/vendors/general/slick/slick.min.js') }}" type="text/javascript"></script>
+<script>
+    $('.home-banner__slick').slick({
+        autoplay: true,
+    });
+
+    onNavigateBanner();
+
+    function onNavigateBanner() {
+        $('.slider-button.slider-button--prev').on('click', function() {
+            $('.slick-prev.slick-arrow').trigger('click');
+        });
+
+        $('.slider-button.slider-button--next').on('click', function() {
+            $('.slick-next.slick-arrow').trigger('click');
+        });
+    }
+</script>
+@endpush
---
+++ resources/views/frontend/pages/home/parials/section-banner.blade.php
@@ -0,0 +1,67 @@
+<section class="shopify-section section">
+    <div class="page-width slideshow-component">
+        <div class="home-banner home-banner__slick">
+            <div class="home-banner-item">
+                <div class="home-banner-item__image">
+                    <img srcset="//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=375 375w,//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=550 550w,//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=750 750w,//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=1100 1100w,//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=1500 1500w,//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=1780 1780w,//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=2000 2000w,//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626 2800w" sizes="100vw" src="//dharmacrafts.com/cdn/shop/files/DC_AutumBestSeller_Slide_17110cdc-994b-4dbc-89a6-b5abfbe96a06.jpg?v=1695393626&amp;width=1500" loading="lazy" alt="" width="2800" height="1170">
+                </div>
+            </div>
+
+            <div class="home-banner-item">
+                <div class="home-banner-item__image">
+                    <img srcset="//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=375 375w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=550 550w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=750 750w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=1100 1100w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=1500 1500w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=1780 1780w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=2000 2000w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=3000 3000w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=3840 3840w,//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660 4472w" sizes="100vw" src="//dharmacrafts.com/cdn/shop/files/custom_resized_38b76b2c-37b4-46db-8237-12375589313c.jpg?v=1691169660&amp;width=1500" loading="lazy" alt="" width="4472" height="2982">
+                </div>
+            </div>
+
+            <div class="home-banner-item">
+                <div class="home-banner-item__image">
+                    <img srcset="//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=375 375w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=550 550w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=750 750w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=1100 1100w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=1500 1500w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=1780 1780w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=2000 2000w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=3000 3000w,//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168 3167w" sizes="100vw" src="//dharmacrafts.com/cdn/shop/files/Screen_Shot_2022-03-04_at_10.18.22_AM.png?v=1646407168&amp;width=1500" loading="lazy" alt="" width="3167" height="2117">
+                </div>
+            </div>
+
+            <div class="home-banner-item">
+                <div class="home-banner-item__image">
+                    <img srcset="//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=375 375w,//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=550 550w,//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=750 750w,//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=1100 1100w,//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=1500 1500w,//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=1780 1780w,//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=2000 2000w,//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183 2800w" sizes="100vw" src="//dharmacrafts.com/cdn/shop/files/DC_EyePillow_Slide_7f5215ec-1565-4060-aa09-fcb3461e417e.jpg?v=1685719183&amp;width=1500" loading="lazy" alt="" width="2800" height="1170">
+                </div>
+            </div>
+
+            <div class="home-banner-item">
+                <div class="home-banner-item__image">
+                    <img srcset="//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=375 375w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=550 550w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=750 750w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=1100 1100w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=1500 1500w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=1780 1780w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=2000 2000w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=3000 3000w,//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280 3000w" sizes="100vw" src="//dharmacrafts.com/cdn/shop/files/Dharma_ColoradoGRigg_2023_LieslClarkPhoto088.jpg?v=1677774280&amp;width=1500" loading="lazy" alt="" width="3000" height="2000">
+                </div>
+            </div>
+        </div>
+        <div class="home-banner__slick-control">
+            <div class="slideshow__controls slideshow__controls--top slider-buttons no-js-hidden">
+                <button type="button" class="slider-button slider-button--prev" name="previous" aria-label="Previous slide" aria-controls="Slider-template--16599720624378__1655078171bc129901"><svg aria-hidden="true" focusable="false" role="presentation" class="icon icon-caret" viewBox="0 0 10 6">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z" fill="currentColor"></path></svg>
+                </button>
+
+                <div class="slider-counter slider-counter--dots">
+                    <div class="slideshow__control-wrapper">
+                        <button class="slider-counter__link slider-counter__link--dots link" aria-label="Load slide 1 of 5" aria-controls="Slider-template--16599720624378__1655078171bc129901">
+                            <span class="dot"></span>
+                        </button>
+                        <button class="slider-counter__link slider-counter__link--dots link slider-counter__link--active" aria-label="Load slide 2 of 5" aria-controls="Slider-template--16599720624378__1655078171bc129901" aria-current="true">
+                            <span class="dot"></span>
+                        </button>
+                        <button class="slider-counter__link slider-counter__link--dots link" aria-label="Load slide 3 of 5" aria-controls="Slider-template--16599720624378__1655078171bc129901">
+                            <span class="dot"></span>
+                        </button>
+                        <button class="slider-counter__link slider-counter__link--dots link" aria-label="Load slide 4 of 5" aria-controls="Slider-template--16599720624378__1655078171bc129901">
+                            <span class="dot"></span>
+                        </button>
+                        <button class="slider-counter__link slider-counter__link--dots link" aria-label="Load slide 5 of 5" aria-controls="Slider-template--16599720624378__1655078171bc129901">
+                            <span class="dot"></span>
+                        </button>
+                    </div>
+                </div>
+                <button type="button" class="slider-button slider-button--next" name="next" aria-label="Next slide" aria-controls="Slider-template--16599720624378__1655078171bc129901">
+                    <svg aria-hidden="true" focusable="false" role="presentation" class="icon icon-caret" viewBox="0 0 10 6">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z" fill="currentColor"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
