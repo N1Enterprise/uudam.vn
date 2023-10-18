50b7d3f2d6553e9a5b81db4b5aa4b82843dfca75
stevephamhi <hung.pham@cubicstack.net>
2023-10-18 08:15:00 +0700

    bo included product p4

--- resources/views/frontend/pages/products/js-pages/index.blade.php
+++ resources/views/frontend/pages/products/js-pages/index.blade.php
@@ -87,15 +87,35 @@ const PRODUCT_VARIANTS = {
             return `
             <div class="included-products__item">
                 <div class="included-products__item-image" title="${item.description}">
-                    <img src="${item.image}" alt="${item.name}" width="40" height="40">
+                    <img src="${item.image}" alt="test">
                 </div>
 
                 <div class="included-products__item-info">
                     <div>
                         <h3 class="included-products-info-name" style="margin: 0">${item.name}</h3>
-                        <span class="included-products-info-price">${__HELPER__.formatNumber(item.sale_price)+' VND'}</span>
+                        <span class="included-products-info-price">${__HELPER__.formatNumber(item.sale_price)}</span>
                     </div>
-                    <div>
+                    <div class="included-products__item-sale">
+                        <div>
+                            <div class="product-form__input product-form__quantity">
+                                <label class="form__label">Số Lượng</label>
+                                <quantity-input class="quantity">
+                                    <button class="quantity__button no-js-hidden" name="minus" type="button">
+                                        <span class="visually-hidden">Decrease quantity for test</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" role="presentation" class="icon icon-minus" fill="none" viewBox="0 0 10 2">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M.5 1C.5.7.7.5 1 .5h8a.5.5 0 110 1H1A.5.5 0 01.5 1z" fill="currentColor"></path>
+                                        </svg>
+                                    </button>
+                                    <input class="quantity__input" type="number" name="quantity" min="1" value="1" max="${item.stock_quantity}">
+                                    <button class="quantity__button no-js-hidden" name="plus" type="button">
+                                        <span class="visually-hidden">Increase quantity for test</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" role="presentation" class="icon icon-plus" fill="none" viewBox="0 0 10 10">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1 4.51a.5.5 0 000 1h3.5l.01 3.5a.5.5 0 001-.01V5.5l3.5-.01a.5.5 0 00-.01-1H5.5L5.49.99a.5.5 0 00-1 .01v3.5l-3.5.01H1z" fill="currentColor"></path>
+                                        </svg>
+                                    </button>
+                                </quantity-input>
+                            </div>
+                        </div>
                         <button type="button" class="included-products-info-order">Mua Kèm</button>
                     </div>
                 </div>
