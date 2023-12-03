1f5fea925a728ad512a93be40ffad57553ed371f
stevephamhi <hung.pham@cubicstack.net>
2023-12-03 23:11:20 +0700

    fix route

--- app/Providers/BackofficeRouteServiceProvider.php
+++ app/Providers/BackofficeRouteServiceProvider.php
@@ -25,7 +25,6 @@ class BackofficeRouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware(['web', 'auth:admin'])
-            ->prefix('backoffice')
             ->namespace($this->namespace)
             ->as('bo.web.')
             ->group(base_path('routes/backoffice/web.php'));
