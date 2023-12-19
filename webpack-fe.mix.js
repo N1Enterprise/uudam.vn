a76d6324d178159907f076b1dd8736e50cdfda90
stevephamhi <hung.pham@cubicstack.net>
2023-12-19 14:15:20 +0700

    fix node max_old_space_size

--- webpack-fe.mix.js
+++ webpack-fe.mix.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix');
 let productionSourceMaps = true;
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,18 @@ mix.webpackConfig({
     output: {
         chunkFilename: 'assets/js/[name].min.js',
         publicPath: '/',
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    // Your Terser options go here
+                    compress: {
+                        drop_console: true,
+                    },
+                },
+            }),
+        ],
     },
 });
 
