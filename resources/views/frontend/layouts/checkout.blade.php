204150cacc3c6e8cb12ec6ebfc74f7e63a7fc08b
stevephamhi <hung.pham@cubicstack.net>
2023-12-28 16:46:34 +0700

    fix product

--- resources/views/frontend/layouts/checkout.blade.php
+++ resources/views/frontend/layouts/checkout.blade.php
@@ -2,7 +2,8 @@
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Thanh toán - {{ data_get($SYSTEM_SETTING, 'page_settings.title') }}</title>
     <meta name="description" content="Latest updates and statistic charts">
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <title>@yield('page_title', __($APP_NAME))</title>
 
-    <meta name="description" content="Latest updates and statistic charts">
+    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
