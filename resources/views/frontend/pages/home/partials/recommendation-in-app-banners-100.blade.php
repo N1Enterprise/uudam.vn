11faef86b2a97cb4c9a17b1bb7f87da1e11e5210
stevephamhi <hung.pham@cubicstack.net>
2024-03-30 10:58:34 +0700

    fix google speed

--- resources/views/frontend/pages/home/partials/recommendation-in-app-banners-100.blade.php
+++ resources/views/frontend/pages/home/partials/recommendation-in-app-banners-100.blade.php
@@ -4,7 +4,7 @@
             @foreach (data_get($item, 'linked_items') as $linkedItem)
             <div data-recommendation-in-app-banner-100-identifier="{{ $linkedItem }}" class="in-app-banner-wrapper">
                 <a href="/" class="a-prevent">
-                    <img class="image-lazy" srcset="{{ asset_with_version('frontend/assets/images/shared/skeleton-banner-100.webp') }}" src="{{ asset_with_version('frontend/assets/images/shared/skeleton-post.webp') }}" sizes="(min-width: 1600px) 750px, (min-width: 750px) calc((100vw - 130px) / 2), calc((100vw - 50px) / 2)" alt="Ưu đàm bài viết" class="motion-reduce" loading="lazy" width="1815" height="150">
+                    <img class="image-lazy" srcset="{{ asset_with_version('frontend/assets/images/shared/skeleton-banner-100.webp') }}" src="{{ asset_with_version('frontend/assets/images/shared/skeleton-post.webp') }}" sizes="(min-width: 1600px) 750px, (min-width: 750px) calc((100vw - 130px) / 2), calc((100vw - 50px) / 2)" alt="Ưu đàm bài viết" class="motion-reduce" loading="lazy">
                 </a>
             </div>
             @endforeach
