67fcaec3dbdee29c776947f3eddcf548b61ac718
stevephamhi <hung.pham@cubicstack.net>
2024-04-11 23:36:19 +0700

    fix css

--- resources/views/frontend/pages/products/partials/product-info.blade.php
+++ resources/views/frontend/pages/products/partials/product-info.blade.php
@@ -156,6 +156,9 @@
                     @endif
 
                     <div style="display: flex; justify-content: space-between;">
+                        <button type="button" id="buy_now" class="product-form__submit button button--full-width button--primary" data-return-url="{{ route('fe.web.user.checkout.confirmation') }}" login-ref="#Add_Cart_Required_Login" style="flex: 1; margin-right: 10px;">
+                            <span>Mua ngay</span>
+                        </button>
                         <a class="link" href="{{ route('fe.web.cart.index') }}" style="margin-bottom: 1rem; border: 1px solid #ccc; display: flex; align-items: center; justify-content: center; text-decoration: none; width: 50px; height: 50px; position: relative;" title="Xem giỏ hàng">
                             <svg class="icon icon-cart" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" fill="none" style="width: 40px; height: 40px;">
                                 <path fill="currentColor" fill-rule="evenodd" d="M20.5 6.5a4.75 4.75 0 00-4.75 4.75v.56h-3.16l-.77 11.6a5 5 0 004.99 5.34h7.38a5 5 0 004.99-5.33l-.77-11.6h-3.16v-.57A4.75 4.75 0 0020.5 6.5zm3.75 5.31v-.56a3.75 3.75 0 10-7.5 0v.56h7.5zm-7.5 1h7.5v.56a3.75 3.75 0 11-7.5 0v-.56zm-1 0v.56a4.75 4.75 0 109.5 0v-.56h2.22l.71 10.67a4 4 0 01-3.99 4.27h-7.38a4 4 0 01-4-4.27l.72-10.67h2.22z"></path>
@@ -170,9 +173,6 @@
                                 </span>
                             </div>
                         </a>
-                        <button type="button" id="buy_now" class="product-form__submit button button--full-width button--primary" data-return-url="{{ route('fe.web.user.checkout.confirmation') }}" login-ref="#Add_Cart_Required_Login" style="flex: 1; margin-left: 10px;">
-                            <span>Mua ngay</span>
-                        </button>
                     </div>
 
                     <button type="submit" name="add" class="product-form__submit button button--full-width button--primary" style="background-color: #fff; color: #000;">
