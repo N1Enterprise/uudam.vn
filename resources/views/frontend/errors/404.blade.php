041517d49e78dba89c3c1be1d642793cef3b8770
stevephamhi <hung.pham@cubicstack.net>
2024-04-13 10:14:43 +0700

    fix 404 page

--- resources/views/frontend/errors/404.blade.php
+++ resources/views/frontend/errors/404.blade.php
@@ -4,7 +4,7 @@
 404 không tìm thấy trang | {{ config('app.user_domain') }}
 @endsection
 
-@section('style_pages')
+@push('style_pages')
 <style type="text/css">
     .template-404 .title + * {
         margin-top: 1rem;
@@ -15,14 +15,19 @@
     }
 }
 </style>
-@endsection
+<link rel="stylesheet" href="{{ asset_with_version('frontend/bundle/css/pages/home.min.css') }}">
+@endpush
 
 @section('content_body')
 <div id="shopify-section-template--16599720198394__main" class="shopify-section">
     <div class="template-404 page-width page-margin center">
-        <p>404</p>
+        <div>
+            <img src="https://cdn.uudam.vn/shared/others/404-khong-tim-thay-trang.jpg" alt="404 không tìm thấy trang này" style="width: 400px; height: auto;">
+        </div>
         <h1 class="title">Trang không tồn tại!</h1>
-        <a href="{{ route('fe.web.home') }}" class="button">Tiếp tục mua sắm</a>
+        <span style="display: block;"><a href="{{ route('fe.web.home') }}" style="color: #025B50;">Uudam.vn</a> xin lỗi vì sự bất tiện này, có thể trang này hiện không còn hoạt động nữa,</span>
+        <span style="display: block;"><a href="{{ route('fe.web.home') }}" style="color: #025B50;">Uudam</a> sẽ xem xét vấn đề đang xảy ra với bạn, Cảm ơn bạn rất nhiều 🙇🙇🙇</span>
+        <a href="{{ route('fe.web.home') }}" class="button" style="margin-top: 20px;">Quay về trang chủ</a>
     </div>
 </div>
 @endsection
