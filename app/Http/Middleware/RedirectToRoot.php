c425ccff5c9ea407fdf09d6394b66180fbd4883f
stevephamhi <hung.pham@cubicstack.net>
2024-04-15 12:00:44 +0700

    force https and replace /public

--- app/Http/Middleware/RedirectToRoot.php
+++ app/Http/Middleware/RedirectToRoot.php
@@ -16,16 +16,18 @@ class RedirectToRoot
      */
     public function handle(Request $request, Closure $next)
     {
-        $enableForceHttps = ! boolean(app()->environment('local'));
+        $enableForceHttps = boolean(env('app.enable_force_https'));
 
         // Check if the request is not already using HTTPS and redirect if not
         if (! $request->secure() && $enableForceHttps) {
             return redirect()->secure($request->getRequestUri());
         }
 
+        $host = $request->getHost();
+
         // Check if the request URI starts with '/public' and redirect to root if so
         if (strpos($request->getRequestUri(), '/public') === 0) {
-            $newUrl = rtrim(config('app.url'), '/') . '/';
+            $newUrl = str_replace($host . '/' . 'public', $host, $request->url());
             return redirect($newUrl);
         }
 
