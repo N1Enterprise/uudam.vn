5117d2a0f241a728f564528d20de8e2a8c62959f
stevephamhi <hung.pham@cubicstack.net>
2024-04-17 00:11:18 +0700

    fix css home

--- resources/views/frontend/pages/home/partials/section-banner.blade.php
+++ resources/views/frontend/pages/home/partials/section-banner.blade.php
@@ -14,29 +14,31 @@
     <div class="slideshow banner banner--medium grid grid--1-col slider slider--everywhere" data-autoplay="true" data-speed="9" data-owl-id="Slider_Home_Banner" data-owl-ignore-nav="true" data-owl-items="1">
         @foreach ($homeBanners as $banner)
         <div class="slideshow__slide grid__item grid--1-col slider__slide" tabindex="-1">
-            <div class="slideshow__media banner__media media">
-                <img data-device="desktop" class="image-lazy" src="{{ data_get($banner, 'desktop_image') }}" srcset="{{ data_get($banner, 'desktop_image') }}" loading="lazy" alt="{{ data_get($banner, 'label') }}">
-                <img data-device="mobile" class="image-lazy" src="{{ data_get($banner, 'mobile_image', data_get($banner, 'desktop_image')) }}" srcset="{{ data_get($banner, 'mobile_image', data_get($banner, 'desktop_image')) }}" loading="lazy" alt="{{ data_get($banner, 'label') }}">
-            </div>
-            <div class="slideshow__text-wrapper banner__content banner__content--middle-center page-width banner--desktop-transparent">
-                <div class="slideshow__text banner__box content-container content-container--full-width-mobile color-accent-1 gradient slideshow__text--center slideshow__text-mobile--center">
-                    @if (data_get($banner, 'label'))
-                    <h2 class="banner__heading h1" style="color: {{ data_get($banner, 'color', '#ffffff') }}">{{ data_get($banner, 'label') }}</h2>
-                    @endif
-
-                    @if (data_get($banner, 'description'))
-                    <div class="banner__text">
-                        <span style="color: {{ data_get($banner, 'color', '#ffffff') }}">{{ data_get($banner, 'description') }}</span>
-                    </div>
-                    @endif
-
-                    @if (data_get($banner, 'redirect_url') && data_get($banner, 'cta_label'))
-                    <div class="banner__buttons">
-                        <a href="{{ data_get($banner, 'redirect_url') }}" data-color="{{ data_get($banner, 'color', '#ffffff') }}" class="button-banner button--primary" tabindex="-1" style="background: none; color: {{ data_get($banner, 'color', '#ffffff') }}; border: 1px solid {{ data_get($banner, 'color', '#ffffff') }}">{{ data_get($banner, 'cta_label') }}</a>
-                    </div>
-                    @endif
+            <a href="{{ data_get($banner, 'redirect_url') }}" style="display: block; text-decoration: none;">
+                <div class="slideshow__media banner__media media">
+                    <img data-device="desktop" class="image-lazy" src="{{ data_get($banner, 'desktop_image') }}" srcset="{{ data_get($banner, 'desktop_image') }}" loading="lazy" alt="{{ data_get($banner, 'label') }}">
+                    <img data-device="mobile" class="image-lazy" src="{{ data_get($banner, 'mobile_image', data_get($banner, 'desktop_image')) }}" srcset="{{ data_get($banner, 'mobile_image', data_get($banner, 'desktop_image')) }}" loading="lazy" alt="{{ data_get($banner, 'label') }}">
                 </div>
-            </div>
+                <div class="slideshow__text-wrapper banner__content banner__content--middle-center page-width banner--desktop-transparent">
+                    <div class="slideshow__text banner__box content-container content-container--full-width-mobile color-accent-1 gradient slideshow__text--center slideshow__text-mobile--center">
+                        @if (data_get($banner, 'label'))
+                        <h2 class="banner__heading h1" style="color: {{ data_get($banner, 'color', '#ffffff') }}">{{ data_get($banner, 'label') }}</h2>
+                        @endif
+
+                        @if (data_get($banner, 'description'))
+                        <div class="banner__text">
+                            <span style="color: {{ data_get($banner, 'color', '#ffffff') }}">{{ data_get($banner, 'description') }}</span>
+                        </div>
+                        @endif
+
+                        @if (data_get($banner, 'redirect_url') && data_get($banner, 'cta_label'))
+                        <div class="banner__buttons">
+                            <span data-color="{{ data_get($banner, 'color', '#ffffff') }}" class="button-banner button--primary" tabindex="-1" style="background: none; color: {{ data_get($banner, 'color', '#ffffff') }}; border: 1px solid {{ data_get($banner, 'color', '#ffffff') }}">{{ data_get($banner, 'cta_label') }}</span>
+                        </div>
+                        @endif
+                    </div>
+                </div>
+            </a>
         </div>
         @endforeach
     </div>
