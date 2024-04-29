9eebdf748eb0af0da5cdd3ffe5cbb4499f709d3d
stevephamhi <hung.pham@cubicstack.net>
2024-04-29 21:09:43 +0700

    fix node build

--- webpack-fe.mix.js
+++ webpack-fe.mix.js
@@ -99,7 +99,7 @@ mix
     // .css('resources/src/frontend/css/master.css', 'public/frontend/bundle/css/master.min.css')
 
     .css('resources/src/frontend/css/pages/blog.css', 'public/frontend/bundle/css/pages/blog.min.css')
-    .css('resources/src/frontend/css/pages/cart.css', 'public/frontend/bundle/css/pages/cart.min.css')
+    // .css('resources/src/frontend/css/pages/cart.css', 'public/frontend/bundle/css/pages/cart.min.css')
     // .css('resources/src/frontend/css/pages/checkout.css', 'public/frontend/bundle/css/pages/checkout.min.css')
     // .css('resources/src/frontend/css/pages/collection.css', 'public/frontend/bundle/css/pages/collection.min.css')
     // .css('resources/src/frontend/css/pages/home.css', 'public/frontend/bundle/css/pages/home.min.css')
