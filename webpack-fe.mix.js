f6a36ebfa527d6a1d04492efd4a3576d106b40be
stevephamhi <hung.pham@cubicstack.net>
2024-04-29 21:08:43 +0700

    fix node build

--- webpack-fe.mix.js
+++ webpack-fe.mix.js
@@ -98,8 +98,8 @@ mix
     .css('resources/src/frontend/css/mobile-menu.css', 'public/frontend/bundle/css/mobile-menu.min.css')
     // .css('resources/src/frontend/css/master.css', 'public/frontend/bundle/css/master.min.css')
 
-    // .css('resources/src/frontend/css/pages/blog.css', 'public/frontend/bundle/css/pages/blog.min.css')
-    // .css('resources/src/frontend/css/pages/cart.css', 'public/frontend/bundle/css/pages/cart.min.css')
+    .css('resources/src/frontend/css/pages/blog.css', 'public/frontend/bundle/css/pages/blog.min.css')
+    .css('resources/src/frontend/css/pages/cart.css', 'public/frontend/bundle/css/pages/cart.min.css')
     // .css('resources/src/frontend/css/pages/checkout.css', 'public/frontend/bundle/css/pages/checkout.min.css')
     // .css('resources/src/frontend/css/pages/collection.css', 'public/frontend/bundle/css/pages/collection.min.css')
     // .css('resources/src/frontend/css/pages/home.css', 'public/frontend/bundle/css/pages/home.min.css')
