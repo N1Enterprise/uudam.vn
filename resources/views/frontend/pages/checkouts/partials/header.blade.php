17c3f8f9d5c417e94cb232d375658249bed67695
stevephamhi <hung.pham@cubicstack.net>
2024-05-10 20:37:51 +0700

    fix css

--- resources/views/frontend/pages/checkouts/partials/header.blade.php
+++ resources/views/frontend/pages/checkouts/partials/header.blade.php
@@ -1,4 +1,3 @@
-<div class="wrap">
     <header>
         <a href="{{ route('fe.web.home') }}" class="s2kwpi1 _1frageme0 _1fragemlr _1fragemm0 s2kwpi3 _1fragemli _1fragemlm">
             <img
@@ -10,12 +9,8 @@
             >
         </a>
         <a id="cart-link" href="{{ route('fe.web.cart.index') }}">
-            <span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" focusable="false">
-                    <path d="M2.675 10.037 3.072 4.2h7.856l.397 5.837A2.4 2.4 0 0 1 8.931 12.6H5.069a2.4 2.4 0 0 1-2.394-2.563Z"></path>
-                    <path d="M4.9 3.5a2.1 2.1 0 1 1 4.2 0v1.4a2.1 2.1 0 0 1-4.2 0V3.5Z"></path>
-                </svg>
-            </span>
+            <svg class="icon icon-cart" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" fill="none" style="width: 50px; height: 50px; stroke: #fff;">
+                <path fill="currentColor" fill-rule="evenodd" d="M20.5 6.5a4.75 4.75 0 00-4.75 4.75v.56h-3.16l-.77 11.6a5 5 0 004.99 5.34h7.38a5 5 0 004.99-5.33l-.77-11.6h-3.16v-.57A4.75 4.75 0 0020.5 6.5zm3.75 5.31v-.56a3.75 3.75 0 10-7.5 0v.56h7.5zm-7.5 1h7.5v.56a3.75 3.75 0 11-7.5 0v-.56zm-1 0v.56a4.75 4.75 0 109.5 0v-.56h2.22l.71 10.67a4 4 0 01-3.99 4.27h-7.38a4 4 0 01-4-4.27l.72-10.67h2.22z"></path>
+            </svg>
         </a>
     </header>
-</div>
