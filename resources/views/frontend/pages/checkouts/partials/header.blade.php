592344be5a921b3006b6f57541464e9203bb50a5
stevephamhi <hung.pham@cubicstack.net>
2024-05-10 13:06:49 +0700

    fix fe order page

--- resources/views/frontend/pages/checkouts/partials/header.blade.php
+++ resources/views/frontend/pages/checkouts/partials/header.blade.php
@@ -13,10 +13,12 @@
         </div>
         <div>
             <a id="cart-link" href="{{ route('fe.web.cart.index') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" focusable="false" class="a8x1wuo _1fragemfc _1fragemhb _1fragemd8 _1fragemd4">
-                    <path d="M2.675 10.037 3.072 4.2h7.856l.397 5.837A2.4 2.4 0 0 1 8.931 12.6H5.069a2.4 2.4 0 0 1-2.394-2.563Z"></path>
-                    <path d="M4.9 3.5a2.1 2.1 0 1 1 4.2 0v1.4a2.1 2.1 0 0 1-4.2 0V3.5Z"></path>
-                </svg>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" focusable="false" class="a8x1wuo _1fragemfc _1fragemhb _1fragemd8 _1fragemd4">
+                        <path d="M2.675 10.037 3.072 4.2h7.856l.397 5.837A2.4 2.4 0 0 1 8.931 12.6H5.069a2.4 2.4 0 0 1-2.394-2.563Z"></path>
+                        <path d="M4.9 3.5a2.1 2.1 0 1 1 4.2 0v1.4a2.1 2.1 0 0 1-4.2 0V3.5Z"></path>
+                    </svg>
+                </span>
             </a>
         </div>
     </header>
