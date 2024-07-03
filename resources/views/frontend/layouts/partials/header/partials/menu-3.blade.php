8bb1e6592dced3e9f645c9deb2868c6898b40d40
stevephamhi <hung.pham@cubicstack.net>
2024-07-03 22:02:53 +0700

    [fe] fix

--- resources/views/frontend/layouts/partials/header/partials/menu-3.blade.php
+++ resources/views/frontend/layouts/partials/header/partials/menu-3.blade.php
@@ -5,11 +5,11 @@
 <div data-menu-type="post">
     <div class="mega-menu-item-container">
         <div class="mm-image-container" style="height: 298px;">
-            <div class="mm-image" style="width: 570px; height: 298.359px;">
+            <div class="mm-image" style="width: 450px; height: 298.359px;">
                 <a data-href="{{ route('fe.web.posts.index', data_get($post, 'slug')) }}" href="{{ route('fe.web.posts.index', data_get($post, 'slug')) }}">
                     <img data-src="{{ data_get($post, 'image') }}" alt="{{ data_get($menu, 'name', data_get($post, 'name', 'menu item')) }}" src="{{ data_get($post, 'image') }}" aspect-ratio="1.9132569558101473" class="get-article-image mmLs-is-cached mmLazyloaded" data-id="587642110202"></a>
                 </div>
-            <div class="mm-label-wrap" style="width: 570px; height: 298.359px;"></div>
+            <div class="mm-label-wrap" style="width: 450px; height: 298.359px;"></div>
         </div>
         <a data-href="{{ route('fe.web.posts.index', data_get($post, 'slug')) }}" href="{{ route('fe.web.posts.index', data_get($post, 'slug')) }}" class="mm-featured-title" style="min-height: 18px;">
             <span class="mm-title">
